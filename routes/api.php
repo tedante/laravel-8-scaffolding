d787baa81782782ae36e4087fc75d1741e770279
Teddy K <teddy@communic8agency.com>
2020-09-15 18:13:47 +0700

    login and register done

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function() {
+
+    // Route::post('upload', 'StorageController@upload');
+
+    Route::middleware(['json'])->group(function() {
+        Route::post('login', [AuthController::class, 'login'])->name('login');
+        Route::post('register', [AuthController::class, 'register'])->name('register');
+      
+        Route::middleware('auth:api')->group(function() {
+            
+        });
+    });
+
 });
+
