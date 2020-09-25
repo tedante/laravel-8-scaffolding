cad6db9effa118c512c275aa3731a71b45a53cc6
Teddy K <teddykoerniadi@gmail.com>
2020-09-25 18:50:54 +0700

    add layout header

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -33,6 +33,8 @@
     <div id="content-wrapper" class="d-flex flex-column">
 
       <!-- Main Content -->
+      @include('admin.layouts.header')
+      
       @yield('content')
       <!-- End of Main Content -->
 
