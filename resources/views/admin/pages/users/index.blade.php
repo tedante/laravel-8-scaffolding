b5d6f4ee946cc2f7022f7cf6dcc3b08dede778f1
Teddy K <teddykoerniadi@gmail.com>
2020-09-28 14:06:43 +0700

    add button add new users

--- resources/views/admin/pages/users/index.blade.php
+++ resources/views/admin/pages/users/index.blade.php
@@ -3,15 +3,24 @@
 
 @section('content')
         <!-- Page Heading -->
-{{-- <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-<p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p> --}}
+<h1 class="h3 mb-2 text-gray-800">Users</h1>
+<hr />
+{{-- <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p> --}}
 
 <!-- DataTales Example -->
 <div class="card">
   <div class="card-header">
-    <h6 class="font-weight-bold text-primary">Users</h6>
+    <div class="d-flex justify-content-between">
+      <div class="p-2">
+        <a class="btn btn-info" href="#" title="Add New">
+          Add New
+        </a>
+      </div>
+    </div>
   </div>
   <div class="card-body">
+    
+    <hr />
     <div class="table-responsive">
       <table class="table table-hover" id="datatable" width="100%">
         <thead>
